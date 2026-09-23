7a594fc3cfc4371aeb7da06383cba6d4523e0274
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-23 13:54:34 +0200

    Bind Render service to PORT environment variable

--- rental_hunter/__main__.py
+++ rental_hunter/__main__.py
@@ -1,4 +1,9 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("rental_hunter.server:app", host="0.0.0.0", port=8000)
+    uvicorn.run(
+        "rental_hunter.server:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", "8000")),
+    )
