4737cfda87db640cb52c74d1063f9622e42e1318
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-23 13:54:28 +0200

    Fix Python entrypoint line breaks

--- rental_hunter/__main__.py
+++ rental_hunter/__main__.py
@@ -1,1 +1,4 @@
-import uvicorn\n\nif __name__=="__main__":\n    uvicorn.run("rental_hunter.server:app",host="0.0.0.0",port=8000)\n
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("rental_hunter.server:app", host="0.0.0.0", port=8000)
